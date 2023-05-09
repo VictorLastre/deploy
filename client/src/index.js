1353e9a0afddb2186589bfd76a44145ea316ebdf
VictorLastre <117042793+VictorLastre@users.noreply.github.com>
2023-05-09 14:30:35 -0300

    4 commit

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import axios from 'axios';
 
-axios.defaults.baseURL= "http://localhost:3000"
+axios.defaults.baseURL= "http://localhost:3001"
 
 ReactDOM.render(
   <Provider store={store}>
